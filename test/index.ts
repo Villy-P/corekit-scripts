d741517ff8888694f59904d70a4482b2c72cc35d
Villy-P <valeriuspetrini@gmail.com>
2026-03-22 19:02:32 -0500

    feat: implement shuffle function and update index for export

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,4 @@
-import { greet } from "@valerius_petrini/corekit-scripts";
+import { shuffle } from "@valerius_petrini/corekit-scripts";
 
-console.log(greet("World"));
+const myArray = [1, 2, 3, 4, 5];
+console.log(shuffle(myArray));
